f13ba160bf803bcaa180f7504989b9ea60c85b15
Kreggy <filip.makrlik@gmail.com>
2020-01-15 22:22:18 +0100

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LoginForm';
+  private username = '';
+  private password = '';
+  private correct = '';
+  private incorrect = '';
+  private hide = false;
+
+  buttonClick() {
+    if(this.username === 'Kreggy' && this.password === 'heslo666') {
+      this.correct = 'Logged in successfully';
+      this.hide = true;
+    }
+    else {
+      this.incorrect = 'Wrong username or pasword';
+    }
+  }
 }
